c5f40c6d74130004a77f0b38fcc25c016a47370d
ShawnN003 <Nguru.Shawn@student.greenriver.edu>
2025-04-10 11:32:16 -0700

    while loop implemented

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,22 @@
 public class Practice {
     public static void main(String[] args) 
     {
-        System.out.println("~linked lists practice~");
+        Node myNode = new Node('A', null);
+
+        Node anotherNode = new Node('X', myNode);
+
+        Node anything = new Node('V', anotherNode);
+        
+        Node something = new Node('Q', anything);
+        
+        
+
+        Node current = something;
+
+        while(current.next != null)
+        {
+            System.out.println(current.data);
+            current = current.next;
+        }
     }
 }
