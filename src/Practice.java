99fc4e12015cb758fc89675f71b8b35153f90624
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-04-10 11:23:30 -0700

    current

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,12 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node myNode = new Node('A', null);
+        Node anotherNode = new Node('x', myNode);
+        Node anything = new Node('V', anotherNode);
+        Node something = new Node('Q', anything);
+
+        Node curent = something;
+
+        System.out.println(anotherNode.next.data);
     }
 }
