c1908de682d3ffa33e5b8de0ea39f2c83487c65a
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-04-10 11:49:20 -0700

    Completed Node Practice.

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,22 @@
 public class Practice {
-    public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+    public static void main(String[] args) 
+    {
+        // Node setup
+        Node myNode = new Node('A', null);
+        Node anotherNode = new Node('X', myNode);
+        Node anything = new Node('V', anotherNode);
+        Node something = new Node('Q', anything);
+
+        // Current Reference
+        Node current = something;
+
+        // Print out
+        while (current != null) 
+        {
+            System.out.println(current.data);
+            current = current.next;
+        }
+
     }
+
 }
