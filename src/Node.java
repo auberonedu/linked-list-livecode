0b7736f504943f6abf83b292a13a861d092c1f0c
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-04-10 11:44:31 -0700

    added content to files from class

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,12 @@
 public class Node {
-    
+    //fields
+    public char data;
+    public Node next;
+
+    //constructor(s)
+    public Node(char data, Node next)
+    {
+        this.data = data;
+        this.next = next;
+    }
 }
