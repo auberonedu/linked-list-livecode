64b6e23352edabb79e5d91c8da7a32e126c74a21
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-04-10 11:47:38 -0700

    finished livecode

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,12 @@
 public class Node {
     
-}
+public char dataValue;
+public Node nextNode; //points at the next node, final node points to null
+
+
+public Node (char value, Node next){
+    this.dataValue = value;
+    this.nextNode = next;
+} //end public Node
+
+}//end class Node
