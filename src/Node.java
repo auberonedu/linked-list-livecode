9f10a0a2e1e928cf3704d0ea3ab719a242a284da
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-04-10 11:48:16 -0700

    In-class work

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,9 @@
 public class Node {
-    
+    public Node next;
+    public char data;
+
+    public Node(char data, Node next){
+        this.next = next;
+        this.data = data;
+    }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,18 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println("~linked lists practice~");
+
+        Node myNode = new Node('A', null);
+        Node anotherNode = new Node('X', myNode);
+        Node aNode = new Node('V', anotherNode);
+        Node theNode = new Node('Q', aNode);
+
+        Node cur = theNode;
+
+        while (cur != null) {
+            System.out.println(cur.data);
+            cur = cur.next;
+        }
+        
     }
 }
