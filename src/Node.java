eaf3f40791768eb04fdb63ea7e6ac0762224a861
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-04-10 11:47:58 -0700

    livecode complete

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,12 @@
 public class Node {
-    
+    public char data;
+    public Node next;
+
+    /*
+     * Constructor that's being used in line 3 in Practice.java
+     */
+    public Node(char data, Node next) {
+        this.data = data;       // this.data = Character.toLowerCase(value); || great example of how its not just "this.data = data" or "this.data = value"
+        this.next = next;
+    }
 }
