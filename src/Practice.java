7ee3fc47401905da0dd75782e4d022647409b663
rjfredr <fredrickalexander1727@gmail.com>
2025-04-10 12:29:51 -0700

    in class excercise

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,14 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node myNode = new Node('a',null);
+        Node newNode = new Node('x', myNode);
+        Node newerNode = new Node('v', newNode);
+        Node newestNode = new Node('q', newerNode);
+
+        Node curr = newestNode;
+        while (curr!=null) {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
     }
 }
