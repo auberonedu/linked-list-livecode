8d415d183dd3b2a545ec0592c9eebe18a0ad74b1
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-04-15 11:51:14 -0700

    day 2

--- src/Practice.java
+++ src/Practice.java
@@ -1,15 +1,22 @@
 public class Practice {
     public static void main(String[] args) {
-        Node myNode = new Node('A', null);
-        Node newNode = new Node ('x', myNode);
-        Node againNode = new Node ('V', newNode);
-        Node finalNodeHopefully = new Node ('Q', againNode);
+        MyLinkedList list = new MyLinkedList();
+        list.addToFront('W');
+        list.addToFront('X');
+        list.addToFront('Y');
+        list.addToFront('Z');
+        System.out.println(list);
+        System.out.println(list.get(2));
+        // Node myNode = new Node('A', null);
+        // Node newNode = new Node ('x', myNode);
+        // Node againNode = new Node ('V', newNode);
+        // Node finalNodeHopefully = new Node ('Q', againNode);
         
-        Node current = finalNodeHopefully;
+        // Node current = finalNodeHopefully;
 
-        while(current != null){
-            System.out.println(current.data);
-            current = current.next;
-        }
+        // while(current != null){
+        //     System.out.println(current.data);
+        //     current = current.next;
+        // }
     }
 }
